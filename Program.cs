49df29147a89b1c45c1a9918293e9e61d1c10ad9
ahepra@2020spaces.com <Prabhakar.Aher2020spaces.com>
2022-12-06 11:14:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Interface
+interface IAnimal
+{
+    void animalSound(); // interface method (does not have a body)
+}
+
+// Cat "implements" the IAnimal interface
+class Cat : IAnimal
+{
+    public void animalSound()
+    {
+        // The body of animalSound() is provided here
+        Console.WriteLine("The cat says: meyav meyav");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat Kitti = new Cat();  // Create a Cat object
+        Kitti.animalSound();
+    }
+}
